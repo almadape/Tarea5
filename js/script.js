f522100850d6bc3b3ca90ed00ecc25d9949f51b0
almadape <pedro-819@live.com>
2016-09-04 13:23:13 -0400

    Pedro Almada

---
+++ js/script.js
@@ -0,0 +1,14 @@
+(function () {
+//esto es una funcion anonima
+  console.log('Hola mundo'); //impresion en consola
+}());
+var miarray = []; // esto es una varible tipo array
+var letra = 'string'; //una variable tipo string
+var minumero = 10; //variable numerica
+miarray.push('Primer elemento'); //elemento de array
+miarray.push('Segundo elemento'); //elemento de array
+console.log(miarray); //impresion en consola de array
+var miObjeto = {}; // variable tipo object
+miObjeto.descripcion = 'calzados'; //atributos
+miObjeto.cantidad = 15; //atributos
+console.log(miObjeto);
